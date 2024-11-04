01905164d57ee8345367660a38ee98c347ca47cd
bimagaminh <bimagaminh@gmail.com>
2024-11-04 01:36:24 +0000

    p

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:project_mbkm/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
